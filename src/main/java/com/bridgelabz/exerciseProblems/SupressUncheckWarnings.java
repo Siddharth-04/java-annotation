cee3f9d1bb08b2b6d39a76856b620bfd783c1a03
Siddharth <siddharth1360.be21@chitkara.edu.in>
2025-04-07 09:45:56 +0530

    [Siddharth] ADD: Supress unchecked warnings

---
+++ src/main/java/com/bridgelabz/exerciseProblems/SupressUncheckWarnings.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.exerciseProblems;
+
+import java.util.ArrayList;
+
+public class SupressUncheckWarnings {
+    @SuppressWarnings("unchecked")
+    public static void main(String[] args) {
+        SupressUncheckWarnings obj = new SupressUncheckWarnings();
+        ArrayList list = new ArrayList();
+    }
+}
